8b87d743e91ffecc54d54142629c34ed52b07f86
Tank Yang <tankyang@xrex.io>
2024-12-20 16:41:19 +0800

    chore: add remaining files and styles

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000, // 指定端口為 3000
+    strictPort: true, // 如果端口被占用，不會自動嘗試下一個可用端口
+    open: true // 自動打開瀏覽器
+  }
 })
